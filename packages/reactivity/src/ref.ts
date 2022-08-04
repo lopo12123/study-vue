0c275d5ab2a0ea830874e1f0842d5114a9341d6d
lopo <lopo@zju.edu.cn>
2022-08-04 23:37:30 +0800

    feat: ref

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,4 +1,4 @@
-import { isObject } from "@minivue/shared";
+import { isArray, isObject } from "@minivue/shared";
 import { reactive } from "./reactive";
 import { ReactiveEffect, trackEffects, triggerEffects } from "./effect";
 
@@ -33,6 +33,54 @@ const ref = (value) => {
     return new RefImpl(value)
 }
 
+class ObjectRefImpl {
+    constructor(public object, public key) {
+    }
+
+    get value() {
+        return this.object[this.key]
+    }
+
+    set value(newValue) {
+        this.object[this.key] = newValue
+    }
+}
+
+const toRef = (object, key) => {
+    return new ObjectRefImpl(object, key)
+}
+
+const toRefs = (object) => {
+    const result = isArray(object) ? new Array(object.length) : {}
+
+    for (let key in object) {
+        result[key] = toRef(object, key)
+    }
+
+    return result
+}
+
+const proxyRefs = (object) => {
+    return new Proxy(object, {
+        get(target: any, p: string | symbol, receiver: any): any {
+            const r = Reflect.get(target, p, receiver)
+            return r.__v_isRef ? r.value : r
+        },
+        set(target: any, p: string | symbol, value: any, receiver: any): boolean {
+            const oldValue = target[p]
+            if(oldValue.__v_isRef) {
+                oldValue.value = value
+                return true
+            }
+            else {
+                return Reflect.set(target, p, receiver)
+            }
+        }
+    })
+}
 export {
-    ref
+    ref,
+    toRef,
+    toRefs,
+    proxyRefs,
 }
