207fc2c83a9077051e1be40181f1fffd8fb2c3c9
lopo <lopo@zju.edu.cn>
2022-09-07 22:40:29 +0800

    feat: vnode

---
+++ packages/runtime-core/src/vnode.ts
@@ -0,0 +1,37 @@
+import { isArray, isString, ShapeFlags } from "@minivue/shared";
+
+export const isVnode = (value: any) => {
+    return !!(value?.__v_isVnode)
+}
+
+// 虚拟节点有很多: 组件、元素、文本 ...
+export const createVNode = (type, props, children) => {
+    // 组合方案 shapeFlag 想知道一个元素中包含的是多个子节点还是一个子节点
+    let shapeFlag = isString(type) ? ShapeFlags.ELEMENT : 0
+
+    // 虚拟dom就是一个对象, diff算法 (真实dom的属性较多)
+    const vnode = {
+        type,
+        props,
+        children,
+        // 对应的真实节点, 后续diff算法 => 比对后更新
+        el: null,
+        key: props?.key,
+        __v_isVnode: true,
+        shapeFlag
+    }
+
+    if(children) {
+        let type = 0
+        if(isArray(children)) {
+            type = ShapeFlags.ARRAY_CHILDREN
+        }
+        else {
+            children = String(children)
+            type = ShapeFlags.TEXT_CHILDREN
+        }
+        vnode.shapeFlag |= type
+    }
+
+    return vnode
+}
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,15 +1,26 @@
-const isObject = (value: any) => {
+export const isObject = (value: any) => {
     return typeof value === 'object' && value !== null
 }
-const isFunction = (value: any) => {
+export const isFunction = (value: any) => {
     return typeof value === 'function'
 }
-const isArray = Array.isArray
-const assign = Object.assign
+export const isArray = Array.isArray
+export const isString = (value: any) => {
+    return typeof value === 'string'
+}
+export const assign = Object.assign
 
-export {
-    isObject,
-    isFunction,
-    isArray,
-    assign
+// vue3 提供的形状标识
+export const enum ShapeFlags {
+    ELEMENT = 1,
+    FUNCTIONAL_COMPONENT = 1 << 1,
+    STATEFUL_COMPONENT = 1 << 2,
+    TEXT_CHILDREN = 1 << 3,
+    ARRAY_CHILDREN = 1 << 4,
+    SLOTS_CHILDREN = 1 << 5,
+    TELEPORT = 1 << 6,
+    SUSPENSE = 1 << 7,
+    COMPONENT_SHOULD_KEEP_ALIVE = 1 << 8,
+    COMPONENT_KEPT_ALIVE = 1 << 9,
+    COMPONENT = ShapeFlags.STATEFUL_COMPONENT | ShapeFlags.FUNCTIONAL_COMPONENT,
 }
